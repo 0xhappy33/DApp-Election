401dcd69376db44e3ac69d5f70dac1ef30f8d0ea
truongvanha <hachange.sgroup@gmail.com>
2018-08-13 14:59:16 +0700

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.19;
+
+/**
+ * Created by Ha Truong
+ * Created at August 13rd, 2018
+ * The Election contract does this and that...
+ */
+contract Election {
+
+	string public candidate;
+
+	function Election () {
+		candidate = "Candidate 1";
+	}	
+}
+
